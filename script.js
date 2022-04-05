5c84d54da89bcb45fa3a6a69e9189ab1b69fb122
Driijko <driijko@gmail.com>
2022-04-05 15:26:12 -0400

    Test leaf 2 on mobile.

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function layout () {
       let width = ((0.5625 * window.innerHeight) / window.innerWidth) * window.innerWidth;
       document.documentElement.style.setProperty("--w", `${width}px`);
       document.documentElement.style.setProperty("--h", `${window.innerHeight}px`);
-      document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      // document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      document.documentElement.style.setProperty("--gh", window.innerHeight + 1);
       document.documentElement.style.setProperty("--gw", `${((window.innerWidth - width)/2) + 1}px`);
       document.documentElement.style.setProperty("--vg", "none");
       document.documentElement.style.setProperty("--hg", "block");
@@ -19,7 +20,8 @@ function layout () {
     else {
       document.documentElement.style.setProperty("--h", `${window.innerWidth * 1.777}px`);
       document.documentElement.style.setProperty("--w", `${window.innerWidth}px`);
-      document.documentElement.style.setProperty("--gh", `${((window.innerHeight - (window.innerWidth * 1.777))/2) + 1}px`);
+      // document.documentElement.style.setProperty("--gh", `${((window.innerHeight - (window.innerWidth * 1.777))/2) + 1}px`);
+      document.documentElement.style.setProperty("--gh", ((window.innerHeight - (window.innerWidth * 1.777))/2) + 1);
       document.documentElement.style.setProperty("--gw", `${window.innerWidth + 1}px`);
       document.documentElement.style.setProperty("--vg", "block");
       document.documentElement.style.setProperty("--hg", "none");
@@ -31,7 +33,8 @@ function layout () {
       let height = ((0.625 * window.innerWidth) / window.innerHeight) * window.innerHeight;
       document.documentElement.style.setProperty("--h", `${height}px`);
       document.documentElement.style.setProperty("--w", `${window.innerWidth}px`);
-      document.documentElement.style.setProperty("--gh", `${((window.innerHeight - height)/2)+1}px`);
+      // document.documentElement.style.setProperty("--gh", `${((window.innerHeight - height)/2)+1}px`);
+      document.documentElement.style.setProperty("--gh", ((window.innerHeight - height)/2)+1);
       document.documentElement.style.setProperty("--gw", `${(window.innerWidth) + 1}px`);
       document.documentElement.style.setProperty("--vg", "block");
       document.documentElement.style.setProperty("--hg", "none");
@@ -40,7 +43,8 @@ function layout () {
       let width = vh * 100 * 1.6;
       document.documentElement.style.setProperty("--h", `${window.innerHeight}px`);
       document.documentElement.style.setProperty("--w", `${width}px`);
-      document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      // document.documentElement.style.setProperty("--gh", `${window.innerHeight + 1}px`);
+      document.documentElement.style.setProperty("--gh", window.innerHeight + 1);
       document.documentElement.style.setProperty("--gw", `${((window.innerWidth - width)/2) + 1}px`);
       document.documentElement.style.setProperty("--vg", "none");
       document.documentElement.style.setProperty("--hg", "block");
